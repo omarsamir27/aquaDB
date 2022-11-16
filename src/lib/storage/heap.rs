907ee13b6dc0b6e8ace1f891cd3715aeecc8c7cb
omar <omarmohameddrive@gmail.com>
2022-11-17 00:14:30 +0200

    heap page prototypes

---
+++ src/lib/storage/heap.rs
@@ -0,0 +1,29 @@
+struct HeapPage{
+
+}
+
+impl RecordPage {
+    fn new() -> Self{
+        todo!()
+    }
+    fn get_field(){
+        todo!()
+    }
+    fn mark_delete(){
+        todo!()
+    }
+    fn get_tuple(){
+        todo!()
+    }
+    fn init_heap(){
+        todo!()
+    }
+    fn insert_tuple(){
+        todo!()
+    }
+    fn vacuum(){
+        todo!()
+    }
+    //fn search()
+
+}
