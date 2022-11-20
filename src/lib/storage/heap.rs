69b8da242eb4c59d21f790023d7673064f5c4dfa
omar <omarmsamir27@gmail.com>
2022-11-20 02:45:35 +0200

    heap page init

--- src/lib/storage/heap.rs
+++ src/lib/storage/heap.rs
@@ -1,12 +1,25 @@
 use crate::common::numerical::ByteMagic;
 use crate::storage::blockid::BlockId;
 use crate::storage::buffermgr::FrameRef;
-use std::intrinsics::size_of;
-use std::io::Bytes;
+
+// #[cfg(target_pointer_width = "32")]
+// const USIZE_LENGTH :usize = 4 ;
+//
+// #[cfg(target_pointer_width = "64")]
+// const USIZE_LENGTH :usize = 4 ;
 
 struct PageHeader {
     space_start: usize,
     space_end: usize,
+}
+
+impl PageHeader {
+    fn new(payload:&[u8]) -> Self{
+        Self{
+            space_start : payload.extract_u16(0) as usize,
+            space_end : payload.extract_u16(2) as usize
+        }
+    }
 }
 
 struct TuplePointer {
@@ -16,13 +29,9 @@ struct TuplePointer {
 
 impl TuplePointer {
     fn read_pointer(payload: &[u8], offset: usize) -> TuplePointer {
-        let offset = payload.extract_usize(offset);
-        let size = payload.extract_u16(offset + size_of::<usize>());
-        TuplePointer { offset, size }
-    }
-    #[inline(always)]
-    fn size() -> usize {
-        size_of::<usize>() + size_of::<u16>() as usize
+        let tuple_offset = payload.extract_u16(offset);
+        let size = payload.extract_u16(offset + 2);
+        TuplePointer { offset: tuple_offset as usize, size }
     }
 }
 
@@ -39,20 +48,15 @@ impl HeapPage {
         let blk = blk.clone();
         let heap_frame = frame.clone();
         let frame_ref = heap_frame.borrow();
-        let usize_size = size_of::<usize>();
-        let header = PageHeader {
-            space_start: frame_ref.page.payload.as_slice().extract_usize(0),
-            space_end: frame_ref.page.payload.as_slice().extract_usize(usize_size),
-        };
-        let mut current_offset = 0_usize;
+        let header = PageHeader::new(frame_ref.page.payload.as_slice());
+        let mut current_offset = 4_usize;
         let mut tuple_pointers: Vec<TuplePointer> = Vec::new();
         if header.space_start != header.space_end {
-            current_offset = 2 * usize_size;
             while current_offset != header.space_start {
                 let pointer =
                     TuplePointer::read_pointer(frame_ref.page.payload.as_slice(), current_offset);
                 tuple_pointers.push(pointer);
-                current_offset += TuplePointer::size();
+                current_offset += 4 ;
             }
         }
         drop(frame_ref);
@@ -80,10 +84,9 @@ impl HeapPage {
         frame.page.payload[offset..(offset+size as usize)].to_vec()
     }
     fn init_heap(frame:FrameRef) {
-        let size = 2*size_of::<usize>();
-        let header = [size.to_ne_bytes(),size.to_ne_bytes()];
-        let header = header.flatten();
-        frame.borrow_mut().page.write_bytes(header,0)
+        let header = [4_u16.to_ne_bytes(),4_u16.to_ne_bytes()];
+        let header = header.concat();
+        frame.borrow_mut().page.write_bytes(header.as_slice(),0)
     }
     fn insert_tuple() {
         todo!()
