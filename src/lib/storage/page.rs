c3268c72cac8b64946565d98a0bdf6839137fb51
Abdallah Aloush <abdallahaloush6@gmail.com>
2022-12-15 22:46:20 +0200

    page documentation

--- src/lib/storage/page.rs
+++ src/lib/storage/page.rs
@@ -1,17 +1,24 @@
 use positioned_io2::WriteAt;
 use std::fmt::{Debug, Formatter, Pointer};
 
+/// A container for disk block to be stored in memory.
+///
+/// Representation of a disk block inside memory.
 pub struct Page {
     pub payload: Vec<u8>,
 }
 
 impl Page {
+    /// Creates an instance of the page with zeros written as the default value
+    ///
+    /// We can think of this as allocating a space in memory to store a disk block.
     pub fn new(page_size: usize) -> Self {
         Page {
             payload: vec![0; page_size],
         }
     }
 
+    /// Writes bytes to the page
     pub fn write_bytes(&mut self, data: &[u8], offset: u64) {
         self.payload.write_at(offset, data).unwrap();
     }
